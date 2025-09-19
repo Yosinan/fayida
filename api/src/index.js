8947a8be382ec7f0a44d8866ce839d552a362037
Yosinan <ymymym4634@example.com>
2025-09-19 20:01:42 +0300

    fix(api): update CORS origin to allow requests from the specified domain

--- api/src/index.js
+++ api/src/index.js
@@ -18,7 +18,7 @@ const corsOptions = {
     origin: 'https://fayida.yosinan.tech',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-device-id', 'x-device-info', 'X-Requested-With'],
 };
 
 app.use(cors(corsOptions));
