b9fd32215b36aed2f04799c512b7e3dd6365cdd9
Yosinan <ymymym4634@example.com>
2025-09-19 19:40:48 +0300

    fix(api): update CORS origin to allow requests from the specified domain

--- api/src/index.js
+++ api/src/index.js
@@ -17,7 +17,9 @@ app.use(helmet());
 // app.use(cors({ origin: true }));
 app.use(cors({
     origin: 'https://fayida.yosinan.tech',
-    credentials: true
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 app.use(morgan('dev'));
 app.use(express.json());
